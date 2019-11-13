248949b752efdc842466c82db4c7173396102503
Myron <dubtwee@gmail.com>
2019-11-13 16:17:59 -0600

    adding code from lesson

--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,31 @@
 console.log('Hello Tyto!!!');
+
+$(document).ready(onReady);
+
+function onReady() {
+    const $heading = $('#firstHello');
+    const $noHeading = $('.noHello').text('NO HELLO!!!');
+    let text = 'RUN';
+    const $ul = $('ul');
+
+
+    $('#firstHello, .noHello').attr('style', 'background: blue;');
+    $('h2').removeClass('noHello');
+
+   $ul.find('.orange').addClass('red-border');
+   $ul.parent().addClass('red-border');
+
+    $ul.empty()
+    $ul.append(`<li>
+            ${text}
+        </li>`)
+        .append(`<li>
+            ${text}
+        </li>`).append(`<li>
+        ${text}
+    </li>`);
+
+    $heading.text(text = 'not run');
+
+    console.log(heading);
+}
